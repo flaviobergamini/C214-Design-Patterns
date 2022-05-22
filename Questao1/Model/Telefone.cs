516c5834bd627a7e63febb7c12452c7c7a72ad7c
flaviobergamini <flaviohenrique@gec.inatel.br>
2022-05-22 16:22:41 -0300

    Criando propriedades para recebimento e tratamento do formato dos números de telefone. Realizando separação de DDD, operadora, fazendo casting para tipos numéricos para facilitar as ordenações

--- Questao1/Model/Telefone.cs
+++ Questao1/Model/Telefone.cs
@@ -4,22 +4,61 @@ namespace Questao1.Model{
 
     class Telefone : Pessoa
     {
-        private int ddd;
-        private int operadora;
-        public string numero{get;}
+        public Telefone(string nome, int anoNasc, string endereco)
+         : base(nome, anoNasc, endereco){}
+
+        private int _ddd;
+        private int _operadora;
+
+        private string _numero = "";
         
         private static string regra = @"^\(([1-9]){2}\)(?:([0-9]{5})|([0-9]{4}))-([0-9]{4})$"; // regex para validar numero (xx)xxxxx-xxxx ou (xx)xxxx-xxxx
-        public Telefone(string nome, int anoNasc, string endereco, string numero)
-         : base(nome, anoNasc, endereco)
-        {
-            this.numero = numero;
+        Regex regex = new Regex(regra);
 
+        public string telefone{
+            get{return _numero;} 
+            set{
+                if(regex.IsMatch(value)){
+                    this._numero = value;
+                    string operadora = "";
+
+                    char [] charNumero = this._numero.ToCharArray();
+
+                    string ddd = charNumero[1].ToString() + charNumero[2].ToString();
+                    this._ddd = int.Parse(ddd);
+
+                    for(int i = (this._numero.IndexOf(")") + 1); i < this._numero.IndexOf("-"); i++){
+                        operadora = operadora + charNumero[i].ToString();
+                    }
+                    this._operadora = int.Parse(operadora);
+
+                }
+                else{
+                    throw new ArgumentException("Telefone no formato inválido");
+                }
+            }
         }
 
         public override void mostraInfo(){
             base.mostraInfo();
-            Console.WriteLine($"Telefone: {this.numero}");
+            Console.WriteLine($"Telefone: {this._numero}");
             Console.WriteLine("-----------------------------------");
+        }
+
+        public int getDDD(){
+            return this._ddd;
+        }
+
+        public int getOperadora(){
+            return this._operadora;
+        }
+
+        public long getNumero(){
+            string num = this._numero.Replace("(", "");
+            num = num.Replace(")", "");
+            num = num.Replace("-", "");
+            long numero = long.Parse(num);
+            return numero;
         }
     }
 }
--- Questao1/Program.cs
+++ Questao1/Program.cs
@@ -6,8 +6,12 @@ namespace Questao1 {
     class Program{
         public static void Main(String [] args){
             Console.WriteLine("---------------Lista Telefônica---------------");
-            Telefone telefone = new Telefone("Flávio", 1998, "Rua 1", "(19)99794-8146");
+            Telefone telefone = new Telefone("Flávio", 1998, "Rua 1");
+            telefone.telefone = "(19)99794-8146";
             telefone.mostraInfo();
+            Console.WriteLine(telefone.getDDD());
+            Console.WriteLine(telefone.getOperadora());
+            Console.WriteLine(telefone.getNumero());
 
         }
     }
